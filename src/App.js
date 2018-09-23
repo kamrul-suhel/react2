4dcb110e75a348a9418c272e55e487c8b846c057
Kamrul Ahmed <kamrul_sscis@hotmail.com>
2018-09-23 11:03:01 +0100

    Event hendling.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,72 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import Person from './Person/Person'
 
 class App extends Component {
-  render() {
+    state = {
+        person : [
+            {name: "ian", age: 35},
+            {name: "mike", age: 36},
+            {name: "Hemm", age: 28},
+            {name: "Kamrul", age: 33},
+        ]
+    }
 
-    return (
-      <div className="App">
-          <h2>Reach Course: </h2>
-          <Person name="Ian" age="35">He is a fullstack developer</Person>
-          <Person name="Mike" age="34"/>
-          <Person name="Hemm" age="28">Hemm is a full stack developer</Person>
-      </div>
-);
-  }
+    changeName = (params) => {
+        this.setState({
+            person : [
+                {name: params, age: 35},
+                {name: "mike", age: 36},
+                {name: "Hemm", age: 28},
+                {name: "Kamrul", age: 32},
+            ]
+        })
+    }
+
+    changedHendler = (event) => {
+        this.setState({
+            person : [
+                {name: event.target.value, age: 35},
+                {name: "mike", age: 36},
+                {name: "Hemm", age: 28},
+                {name: "Kamrul", age: 32},
+            ]
+        })
+    }
+
+
+
+    render() {
+        return (
+            <div className="App">
+                <h2>Reach Course: </h2>
+                <button onClick={() => this.changeName('some params')}>Switch person</button>
+
+                <Person
+                    name={this.state.person[0].name}
+                    age={this.state.person[0].age}
+                >He is a fullstack developer</Person>
+
+                <Person
+                    name={this.state.person[1].name}
+                    age={this.state.person[1].age}
+                >He is a fullstack developer</Person>
+
+                <Person
+                    name={this.state.person[2].name}
+                    age={this.state.person[2].age}
+                    changed={this.changedHendler}
+                    click={this.changeName.bind(this, 'max')}
+                >He is a fullstack developer</Person>
+
+                <Person
+                    name={this.state.person[3].name}
+                    age={this.state.person[3].age}
+                >He is a fullstack developer</Person>
+
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,10 +1,12 @@
-import React, { Component } from 'react'
+import React from 'react'
+import './Person.css'
 
 const person = (props) => {
     return (
-        <div>
-            <h1>Hi I am {props.name}. I am {props.age} years old</h1>
+        <div className="Person">
+            <h1 onClick={props.click}>Hi I am {props.name}. I am {props.age} years old</h1>
             <p>{props.children}</p>
+            <input type="text" onChange={props.changed} value={props.name}/>
         </div>
     )
 }
